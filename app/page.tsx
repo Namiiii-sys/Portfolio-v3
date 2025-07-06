742895d19fca1194df162969416b3761f63ff4b1
Namiiii-sys <Namitamehra207@gmail.com>
2025-07-06 23:51:36 +0530

    demo-3

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ProjectSection from './components/projects'
 import Achievements from './components/Achievements'
 import About1 from './components/About1'
-import Contact from './components/Contact'
+// import Contact from './components/Contact'
 import About from './components/About'
 
 const page = () => {
