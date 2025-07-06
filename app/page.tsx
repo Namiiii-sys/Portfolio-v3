020b962c4055b16b94a2cbc0c25447f1c95d4aaa
Namiiii-sys <Namitamehra207@gmail.com>
2025-07-06 23:46:42 +0530

    demmo

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
-// import ProjectSection from './components/projects'
-// import Achievements from './components/Achievements'
+import ProjectSection from './components/projects'
+import Achievements from './components/Achievements'
 import About1 from './components/About1'
-// import Contact from './components/Contact'
+import Contact from './components/Contact'
 import About from './components/About'
 
 const page = () => {
   return (
     <div>
-      {/* <ProjectSection />
-      <Achievements/> */}
-      {/* <Contact/> */}
+      <ProjectSection />
+      <Achievements/> 
+      <Contact/> 
       <About1/>
       <About/>
     </div>
